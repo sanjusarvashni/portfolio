d9eac70b81ba73e372c65c9b7f2b73af21f9e613
sanjusarvashni <sanjusarvashni@gmail.com>
2024-08-01 15:31:11 +0530

    created folders

---
+++ src/components/app.js
@@ -0,0 +1,21 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+
+
+const App = () => {
+  return (
+    <div className="app-layout">
+         <h1>SANJU SARVASHNI</h1>
+    </div>
+  )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>)
+export default App
+
+
+
+
